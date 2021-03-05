5ff77d57f261a53ba276a79f6a594f36677376f4
Asha(권경민) <powfix@gmail.com>
2021-03-05 18:00:31 +0900

    Add RandomUtils, UuidUtils

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import {StringUtils} from "./src/StringUtils";
+import {UuidUtils} from "./src/UuidUtils";
+import {RandomUtils} from "./src/RandomUtils";
 
 exports.StringUtils = StringUtils;
+exports.UuidUtils = UuidUtils;
+exports.RandomUtils = RandomUtils;
